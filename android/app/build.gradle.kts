a9ad0d8240906cae62ad3f7ab2ddb560d63f05cf
kamennova <mashushvik.2001@gmail.com>
2025-08-05 12:51:13 +0300

    init

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "solid_test_task.solid_test_task"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
